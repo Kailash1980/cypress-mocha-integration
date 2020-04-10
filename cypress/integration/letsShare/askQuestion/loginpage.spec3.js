48fe446f6f18a377861638759cd76d2dcf39bf1c
kailash1980 <kailashpathak@gmail.com>
2020-04-10 20:43:44 +0530

    Change in Ask Quetion test case

--- cypress/integration/letsShare/askQuestion/loginpage.spec3.js
+++ cypress/integration/letsShare/askQuestion/loginpage.spec3.js
@@ -14,14 +14,14 @@ beforeEach('read test data', function () {
 
 describe('Ask Question-Test Suite', function () {
 
-    it("Ask Question into the application with valid crediential", function () {
+    it("Ask Question -into the application with valid crediential", function () {
         loginPage.enetURL()
         loginPage.enterUserNamePassword(this.data001.userName, this.data001.passWord)
         loginPage.verifyHomePageText(this.data001.homepageheadertext)
         loginPage.clickLogout()
     })
 
-    it("Ask Question into the application with Invalid crediential", function () {
+    it("Ask Question - into the application with Invalid crediential", function () {
 
         loginPage.enetURL()
         loginPage.enterUserNamePassword(this.data002.userName, this.data002.passWord)
